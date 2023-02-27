d9962f9ed59d8f66bc0f37a7be4441716f8e39a8
Viktoriyadegt <vika.degtyareva.12@mail.ru>
2023-02-27 16:33:56 +0300

    website layout on grids

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,49 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src={'https://www.pngplay.com/wp-content/uploads/12/Snapchat-Icon-No-Background.png'}/>
+            </header>
+            <nav className={'nav'}>
+                <a href={'Profile'}>
+                    <div>Profile</div>
+                </a>
+                <a href={'Message'}>
+                    <div>Message</div>
+                </a>
+                <a href={'Music'}>
+                    <div>Music</div>
+                </a>
+                <a href={'Settings'}>
+                    <div>Settings</div>
+                </a>
+            </nav>
+            <div className="content">
+                <div>
+                    <img src={'https://images5.alphacoders.com/112/1123013.jpg'}/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    my posts
+                    <div>
+                        new posts
+                    </div>
+                    <div>
+                        <div>
+                            post 1
+                        </div>
+                        <div>
+                            post 2
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
